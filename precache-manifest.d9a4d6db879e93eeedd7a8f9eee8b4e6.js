77ce2126df9683d7775d47c76fb4b7ad47d279d1
Ayelet Peres <aperes7@gmail.com>
2024-05-23 14:59:34 -0400

    Updates

---
+++ precache-manifest.d9a4d6db879e93eeedd7a8f9eee8b4e6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6884584546f071e67e3297fff2c2f679",
+    "url": "/index.html"
+  },
+  {
+    "revision": "384b6e0e0570fd41a474",
+    "url": "/static/js/2.76d42043.chunk.js"
+  },
+  {
+    "revision": "3813662aee1d22298cdc",
+    "url": "/static/js/main.f2642881.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a090c432265fcb1f35d3859c3f861a2b",
+    "url": "/static/media/landing_page.a090c432.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "www.alignair.ai/precache-manifest.b107cf65c264d44c563745602b4dbc40.js"
+  "/precache-manifest.d9a4d6db879e93eeedd7a8f9eee8b4e6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("www.alignair.ai/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
